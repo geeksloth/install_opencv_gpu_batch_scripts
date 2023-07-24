2dea374d119a93cdb4e542afe05ee5172b189e67
GeekSloth <84038443+geeksloth@users.noreply.github.com>
2023-07-24 19:34:48 +0700

    first commit

---
+++ set_env_paths.bat
@@ -0,0 +1,11 @@
+ECHO -- Set these variables below --
+set "openCvSource=C:/OpenCV/opencv-4.6.0"
+set "openCVExtraModules=C:/OpenCV/opencv_contrib-4.x/modules"
+set "openCvBuild=%openCvSource%/build"
+set "toolkitRoot=C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v11.8"
+ECHO -- Set this to the location of your Conda Env
+set "pathToAnaconda=C:/Users/Administrator/anaconda3/envs/py39"
+ECHO -- Leave these variables alone --
+set "buildType=Release"
+set "generator=Ninja"
+set "pyVer=39"
